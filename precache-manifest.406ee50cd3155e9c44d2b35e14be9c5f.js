41178a6cc2722c91eac52e5a051483e5467312c4
Guo, Yizhu <yizhu.guo@commscope.com>
2025-05-21 00:55:40 +0800

    Updates

---
+++ precache-manifest.406ee50cd3155e9c44d2b35e14be9c5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527f61b6ac5d14d2c7e5672b43222877",
+    "url": "/drag-drop-grid/index.html"
+  },
+  {
+    "revision": "c1031bd0bf6bfd06fe7d",
+    "url": "/drag-drop-grid/static/css/2.7ca93def.chunk.css"
+  },
+  {
+    "revision": "7d9acf49be19bfde0267",
+    "url": "/drag-drop-grid/static/css/main.e7ba37a2.chunk.css"
+  },
+  {
+    "revision": "c1031bd0bf6bfd06fe7d",
+    "url": "/drag-drop-grid/static/js/2.5223cb62.chunk.js"
+  },
+  {
+    "revision": "df4f6d4281247f473ea5",
+    "url": "/drag-drop-grid/static/js/3.ca0aac80.chunk.js"
+  },
+  {
+    "revision": "7d9acf49be19bfde0267",
+    "url": "/drag-drop-grid/static/js/main.22662e1a.chunk.js"
+  },
+  {
+    "revision": "e0c53ac7fecb2a37a63c",
+    "url": "/drag-drop-grid/static/js/runtime~main.feeb6c1d.js"
+  },
+  {
+    "revision": "062ecb77d404b39544c6fd7e42035815",
+    "url": "/drag-drop-grid/static/media/chats.062ecb77.png"
+  }
+]);
